fcc66c8007476644185bc8593d7158249bec1240
OxeoH <egor33429@gmail.com>
2023-01-20 02:50:36 +0300

    added card

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -7,13 +7,30 @@ const ProductCard: React.FC = () => {
 
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <div className={styles.imgWrapper}>
-          <img src={catPicture} alt="Cat" />
+    <div className={styles.card}>
+        <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.info}>
+            <div className={styles.title}>Сказочное заморское яство</div>
+            <div className={styles.name}>Нямушка</div>
+            <div className={styles.taste}>с фуа-гра</div>
+            <div className={styles.portion}><span className={styles.count}>10</span> порций</div>
+            <div className={styles.gift}><span className={styles.count}>1</span> мышь в подарок</div>
+          </div>
+          <div className={styles.imgWrapper}>
+            <img src={catPicture} alt="Cat" />
+          </div>
+          <div className={styles.ball}>
+            <div className={styles.weightWrapper}>
+              <div className={styles.weight}>0,5</div>
+              <div className={styles.kilo}>кг</div>
+            </div>
+          </div>
         </div>
       </div>
+      <div className={styles.underText}>Чего сидишь? Порадуй котэ, <a href="/">купи</a></div>
     </div>
+    
   )
 }
 
