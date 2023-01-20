d8378bf1990978c0690ef8a8473b18d127ddbf55
OxeoH <egor33429@gmail.com>
2023-01-20 20:34:36 +0300

    added computed strings

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './ProductCard.module.scss'
 import catPicture from '../../assets/cat.png'
 import { ProductProps } from './product.types'
-import { backColors } from './product.utils'
+import { getColor, getCorrectStringIndex, giftsArray, portionsArray } from './product.utils'
 
 export const ProductCard: React.FC<ProductProps> = ({title, mouses, name, portions, taste, weight, stock}: ProductProps) => {
   const [selected, setSelected] = React.useState(false)
@@ -10,34 +10,28 @@ export const ProductCard: React.FC<ProductProps> = ({title, mouses, name, portio
   const selectProduct = () => {
     setSelected((value) => !value)
   }
-
-  const getColor = () =>{
-    if(stock){
-      if(selected){
-        return backColors.ACTIVE
-      }else{
-        return backColors.PASSIVE
-      }
-    }else{
-      return backColors.OUTSTOCK
-    }
-  }
+  const portionsIndex = getCorrectStringIndex(portions)
+  const giftIndex = getCorrectStringIndex(mouses)
 
   return (
     <div className={styles.card}>
-        <div onClick={() => selectProduct()} style={{backgroundColor: getColor()}} className={styles.container}>
+        <div onClick={() => selectProduct()} style={{backgroundColor: getColor(stock, selected)}} className={styles.container}>
         <div className={styles.wrapper}>
           <div className={styles.info}>
             <div className={styles.title}>{title}</div>
             <div className={styles.name}>{name}</div>
             <div className={styles.taste}>{taste}</div>
-            <div className={styles.portion}><span className={styles.count}>{portions}</span> порций</div>
-            <div className={styles.gift}><span className={styles.count}>{mouses}</span> мышь в подарок</div>
+            <div className={styles.portion}>
+              <span className={styles.count}>{portions}</span> {portionsArray[portionsIndex]}
+            </div>
+            <div className={styles.gift}>
+              <span className={styles.count}>{mouses}</span> {giftsArray[giftIndex]} в подарок
+            </div>
           </div>
           <div className={styles.imgWrapper}>
             <img src={catPicture} alt="Cat" />
           </div>
-          <div className={styles.ball} style={{backgroundColor: getColor()}}>
+          <div className={styles.ball} style={{backgroundColor: getColor(stock, selected)}}>
             <div className={styles.weightWrapper}>
               <div className={styles.weight}>{weight}</div>
               <div className={styles.kilo}>кг</div>
--- src/components/ProductCard/product.utils.ts
+++ src/components/ProductCard/product.utils.ts
@@ -4,3 +4,27 @@ export enum backColors{
     OUTSTOCK="#b3b3b3"
 }
 
+export const portionsArray = ['порция', 'порции','порций']
+export const giftsArray = ['мышь', 'мыши', 'мышей']
+
+export const getColor = (stock: boolean, selected: boolean) =>{
+    if(stock){
+      if(selected){
+        return backColors.ACTIVE
+      }else{
+        return backColors.PASSIVE
+      }
+    }else{
+      return backColors.OUTSTOCK
+    }
+}
+
+export const getCorrectStringIndex = (value: number) => {
+	value = Math.abs(value) % 100; 
+	let num = value % 10;
+	if(value > 10 && value < 20) return 2;
+	if(num > 1 && num < 5) return 1;
+	if(num === 1) return 0; 
+	return 2;
+}
+
